3f00926d917089554f1635592b7666d17306a83d
William Moreno <williamjmorenor@gmail.com>
2022-02-27 01:14:53 +0000

    fix: Heroku postgresql requieres ssl
    Signed-off-by: William Moreno <williamjmorenor@gmail.com>

--- now_lms/__init__.py
+++ now_lms/__init__.py
@@ -99,19 +99,19 @@ CONFIGURACION: Dict = {
     # "SQLALCHEMY_ECHO":True,
 }
 
-# Servicios como Heroku, Elephantsql, Digital Ocean proveen una direccion de corrección que comienza con "postgres"
-# esta va a fallar con SQLAlchemy.
-if not environ.get("DINO") and "postgres:" in CONFIGURACION.get("SQLALCHEMY_DATABASE_URI"):
-    CONFIGURACION["SQLALCHEMY_DATABASE_URI"] = "postgresql+pg8000" + CONFIGURACION.get("SQLALCHEMY_DATABASE_URI")[8:]
 
-# See: 
+# See:
 # - https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-python
 # - https://devcenter.heroku.com/changelog-items/2035
-if environ.get("DINO") and "postgres:" in CONFIGURACION.get("SQLALCHEMY_DATABASE_URI"):
+if (environ.get("DINO")) and ("postgres:" in CONFIGURACION.get("SQLALCHEMY_DATABASE_URI")):
     CONFIGURACION["SQLALCHEMY_DATABASE_URI"] = (
         "postgresql" + CONFIGURACION.get("SQLALCHEMY_DATABASE_URI")[8:] + "?sslmode=require"
     )
-    CONFIGURACION["SQLALCHEMY_DATABASE_URI"] = {}
+
+# Servicios como Heroku, Elephantsql, Digital Ocean proveen una direccion de corrección que comienza con "postgres"
+# esta va a fallar con SQLAlchemy.
+elif "postgres:" in CONFIGURACION.get("SQLALCHEMY_DATABASE_URI"):
+    CONFIGURACION["SQLALCHEMY_DATABASE_URI"] = "postgresql+pg8000" + CONFIGURACION.get("SQLALCHEMY_DATABASE_URI")[8:]
 
 
 # < --------------------------------------------------------------------------------------------- >
