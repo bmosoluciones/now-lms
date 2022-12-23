94bd890060e165f6d3fb693010c8b7944ec9bf15
William José Moreno Reyes <williamjmorenor@gmail.com>
2022-12-23 13:08:44 -0600

    test: Update linter script
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- .lint.sh
+++ .lint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-black now_lms
-mypy now_lms --install-types --non-interactive 
-flake8 now_lms
+python -m pip install --upgrade -r development.txt
+python -m black now_lms
+python -m bandit -r now_lms
+python -m flake8 --ignore=E712 now_lms
+python -m pylint now_lms
+python -m mypy now_lms --install-types --non-interactive 
+python -m curlylint now_lms/templates/
+rm -rf build
 python -m build
 python -m twine check dist/*
-pylint now_lms
+python -m pytest
