29cb0b83f27b8e86006fc67cbb39e57d56c722c1
William José Moreno Reyes <williamjmorenor@gmail.com>
2022-12-23 16:24:05 -0600

    test: Speed up linter
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- .lint.sh
+++ .lint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-python -m pip install --upgrade -r development.txt
 python -m black now_lms
 python -m bandit -r now_lms
 python -m flake8 --ignore=E712 now_lms
 python -m pylint now_lms
 python -m mypy now_lms --install-types --non-interactive 
 python -m curlylint now_lms/templates/
-rm -rf build
-python -m build
-python -m twine check dist/*
 python -m pytest
