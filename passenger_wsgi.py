d5598649f27f4f8b8f749d4b4afb583126789cb3
William José Moreno Reyes <williamjmorenor@gmail.com>
2023-01-26 13:18:22 -0600

    build: Add passenger template
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

---
+++ passenger_wsgi.py
@@ -0,0 +1,11 @@
+# Reference:
+# - https://www.brettsbeta.com/blog/2020/07/flask-on-dreamhost-shared-website-hosting/
+
+import sys, os
+
+INTERP = os.path.join(os.environ["HOME"], "yourdomain.com", "venv", "bin", "python3")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+sys.path.append(os.getcwd())
+
+from now_lms import lms_app as application
