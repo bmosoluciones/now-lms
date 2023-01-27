e0abcd8336be502e95c49bf2ddce4268f1bc0c14
William José Moreno Reyes <williamjmorenor@gmail.com>
2023-01-26 21:27:37 -0600

    build: Python3.7
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(
         "Natural Language :: Spanish",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
