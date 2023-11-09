3fc4194403b502b2e1d78a49e95ae19d52b67a1c
William José Moreno Reyes <williamjmorenor@gmail.com>
2023-11-09 08:32:38 -0600

    test: Adopt ruff as python linter:
    Pylint was taking around a minute to complete, this other tool takes
    one second.
    
    References:
     - https://github.com/astral-sh/ruff
    
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- .lint.sh
+++ .lint.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 echo -------------------------------------------------
-echo Check python code with bandit
+echo Check python code with ruff
 echo -------------------------------------------------
 echo
-python -m bandit -r now_lms
-echo -------------------------------------------------
-echo Check python code with flake8
-echo -------------------------------------------------
-echo
-python -m flake8 --verbose --ignore=E712 now_lms
-echo
-echo -------------------------------------------------
-echo Lint python code with pylint
-echo -------------------------------------------------
-echo
-python -m pylint now_lms
+python -m ruff now_lms
 echo
 echo -------------------------------------------------
 echo Check python types
