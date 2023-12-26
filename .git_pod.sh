a157434aabbe0416cd97e7f2bdb5c2e32345c494
William Moreno <williamjmorenor@gmail.com>
2023-12-26 22:38:59 +0000

    chore: Update gitpod
    Signed-off-by: William Moreno <williamjmorenor@gmail.com>

--- .git_pod.sh
+++ .git_pod.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo apt install -y sqlite
 git config pull.rebase true
-cd ..
-cd ..
-pwd
 cd now_lms/static
 npm install
+cd ..
+cd ..
 python -m pip install --upgrade pip
 python -m pip install --upgrade -r development.txt
 python -m pip install -e .
-/home/gitpod/.pyenv/versions/3.8.12/bin/hupper -m waitress --port=8080 now_lms:app
+hupper -m now_lms
