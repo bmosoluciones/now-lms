d5ab4f8e6fc9298dd84441b72e74b4d9f06a1fba
William José Moreno Reyes <williamjmorenor@gmail.com>
2024-03-03 20:39:19 -0600

    release: v0.0.1a14.dev20240303
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- now_lms/version.py
+++ now_lms/version.py
@@ -54,7 +54,7 @@ REVISION = datetime.today().strftime("%Y%m%d")
 # Release string
 # Refences:
 #  - https://peps.python.org/pep-0440/
-# 0.0.1a14.dev20240302
+# 0.0.1a14.dev20240303
 if PRERELEASE:  # pragma: no cover
     VERSION = MAYOR + "." + MENOR + "." + PATCH + PRERELEASE + ".dev" + REVISION
 else:  # pragma: no cover
