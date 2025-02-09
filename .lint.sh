3c0b7ca832d16dc079cd307d075a38d1b891d78b
William Moreno Reyes CP|MBA <wmoreno@bmogroup.solutions>
2025-02-09 11:42:42 -0600

    build: Update development files
    Signed-off-by: William Moreno Reyes CP|MBA <wmoreno@bmogroup.solutions>

--- .lint.sh
+++ .lint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+pip install --upgrade -r test.txt
+echo -------------------------------------------------
+echo Format Python code
+echo -------------------------------------------------
+echo
+iosrt now_lms
+black now_lms
+echo -------------------------------------------------
+echo Check python code with flake8
+echo -------------------------------------------------
+echo
+flake8 --ignore E501 now_lms
 echo -------------------------------------------------
 echo Check python code with ruff
 echo -------------------------------------------------
@@ -16,3 +28,8 @@ echo Lint html files wiht curlylint
 echo -------------------------------------------------
 echo
 # python -m curlylint now_lms/templates/
+echo -------------------------------------------------
+echo Run tests
+echo -------------------------------------------------
+echo
+CI=True pytest  -v --exitfirst --cov=now_lms --slow=True
