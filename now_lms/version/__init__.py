2c2e2fe9fc2892cbb97c26ffd1c08cc623b6611a
William Moreno <williamjmorenor@gmail.com>
2025-06-28 21:43:39 +0000

    release: v0.0.1a19.dev20250628
    Signed-off-by: William Moreno <williamjmorenor@gmail.com>

--- now_lms/version/__init__.py
+++ now_lms/version/__init__.py
@@ -53,13 +53,13 @@ PRERELEASE = "a19"
 
 # <--------------------------------------------------------------------------> #
 # Date of release
-REVISION = "20250619"
+REVISION = "20250628"
 
 # <--------------------------------------------------------------------------> #
 # Release string
 # Refences:
 #  - https://peps.python.org/pep-0440/
-# 0.0.1a19.dev20250619
+# 0.0.1a19.dev20250628
 if PRERELEASE:
     VERSION = MAYOR + "." + MENOR + "." + PATCH + PRERELEASE + ".dev" + REVISION
 else:
