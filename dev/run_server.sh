10dfcaa89a300359c6f60d836d647a3e01e6fd95
William Moreno <wmoreno@bmogroup.solutions>
2025-07-06 10:24:01 -0600

    fix
    Signed-off-by: William Moreno <wmoreno@bmogroup.solutions>

--- dev/run_server.sh
+++ dev/run_server.sh
@@ -1,1 +1,1 @@
-flask db-reset --with-examples --with-tests && flask run --debug --reload --port 8080 --extra-files "now_lms/templates/"
+flask database reset && flask run --debug --reload --port 8080 --extra-files "now_lms/templates/"
