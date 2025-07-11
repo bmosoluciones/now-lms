aaac351035fde5e376aa3e13c124c65e501f62e9
William Moreno <williamjmorenor@gmail.com>
2025-07-11 19:40:04 +0000

    refactor: Mail Backend
    Signed-off-by: William Moreno <williamjmorenor@gmail.com>

--- now_lms/mail.py
+++ now_lms/mail.py
@@ -19,8 +19,9 @@
 # ---------------------------------------------------------------------------------------
 # Libreria estandar
 # ---------------------------------------------------------------------------------------
-from multiprocessing import Process
+import threading
 from os import environ
+from types import SimpleNamespace
 from typing import TYPE_CHECKING
 
 # ---------------------------------------------------------------------------------------
@@ -33,90 +34,148 @@ from flask_mail import Mail, Message
 # Recursos locales
 # ---------------------------------------------------------------------------------------
 from now_lms.auth import descifrar_secreto
-from now_lms.config import DESARROLLO
 from now_lms.db import MailConfig, database
 from now_lms.logs import log as logger
 
 
 if TYPE_CHECKING:
-    from flask import Flask
+    pass
+
+mail = Mail()
 
 
 # ---------------------------------------------------------------------------------------
 # Load setting from environment variables
 # ---------------------------------------------------------------------------------------
-MAIL_SERVER = environ.get("MAIL_SERVER")
-MAIL_PORT = environ.get("MAIL_PORT")
-MAIL_USE_TLS = environ.get("MAIL_USE_TLS")
-MAIL_USE_SSL = environ.get("MAIL_USE_SSL")
-MAIL_USERNAME = environ.get("MAIL_USERNAME")
-MAIL_PASSWORD = environ.get("MAIL_PASSWORD")
-MAIL_DEFAULT_SENDER = environ.get("MAIL_DEFAULT_SENDER")
+def _load_mail_config_from_env() -> SimpleNamespace:
+    """Carga la configuración de correo electrónico desde las variables de entorno."""
 
-# ---------------------------------------------------------------------------------------
-# Convert string values to boolean
-# ---------------------------------------------------------------------------------------
-if MAIL_USE_SSL == "False" or MAIL_USE_SSL == "false" or MAIL_USE_SSL == "FALSE":
-    MAIL_USE_SSL = False  # type: ignore[assignment]
-elif MAIL_USE_SSL == "True" or MAIL_USE_SSL == "true" or MAIL_USE_SSL == "TRUE":
-    MAIL_USE_SSL = True  # type: ignore[assignment]
-if MAIL_USE_TLS == "False" or MAIL_USE_TLS == "false" or MAIL_USE_TLS == "FALSE":
-    MAIL_USE_TLS = False  # type: ignore[assignment]
-elif MAIL_USE_TLS == "True" or MAIL_USE_TLS == "true" or MAIL_USE_TLS == "TRUE":
-    MAIL_USE_TLS = True  # type: ignore[assignment]
+    logger.trace("Obteniendo configuración de correo electronico desde variables de entorno.")
+    # Server name and user credentials
+    MAIL_SERVER = environ.get("MAIL_SERVER", None)
+    MAIL_PORT = environ.get("MAIL_PORT", None)
+    MAIL_USERNAME = environ.get("MAIL_USERNAME", None)
+    MAIL_PASSWORD = environ.get("MAIL_PASSWORD", None)
+    if MAIL_SERVER and MAIL_PORT and MAIL_USERNAME and MAIL_PASSWORD:
+        logger.debug("Configuración de correo electrónico cargada desde variables de entorno.")
+        mail_configured = True
+        mail_enabled = True
+    else:
+        logger.trace("No se encontró configuración de correo electrónico en variables de entorno.")
+        mail_configured = False
+        mail_enabled = False
+    # TLS/SSL settings
+    MAIL_USE_TLS = environ.get("MAIL_USE_TLS", "False").capitalize()
+    MAIL_USE_SSL = environ.get("MAIL_USE_SSL", "False").capitalize()
+    # Default sender
+    MAIL_DEFAULT_SENDER = environ.get("MAIL_DEFAULT_SENDER")
+
+    # Strin to boolean
+    if MAIL_USE_SSL == "FALSE":
+        MAIL_USE_SSL = False
+    elif MAIL_USE_SSL == "TRUE":
+        MAIL_USE_SSL = True
+    if MAIL_USE_TLS == "FALSE":
+        MAIL_USE_TLS = False
+    elif MAIL_USE_TLS == "TRUE":
+        MAIL_USE_TLS = True
+
+    return SimpleNamespace(
+        mail_configured=mail_configured,
+        mail_enabled=mail_enabled,
+        MAIL_SERVER=MAIL_SERVER,
+        MAIL_PORT=MAIL_PORT,
+        MAIL_USERNAME=MAIL_USERNAME,
+        MAIL_PASSWORD=MAIL_PASSWORD,
+        MAIL_USE_TLS=MAIL_USE_TLS,
+        MAIL_USE_SSL=MAIL_USE_SSL,
+        MAIL_DEFAULT_SENDER=MAIL_DEFAULT_SENDER,
+    )
 
 
-def load_email_setup(flask_app: "Flask"):
-    """Inicia la configuración de correo electronico."""
-
-    logger.trace("Iniciando configuración correo electronico.")
-    with flask_app.app_context():
+def _load_mail_config_from_db() -> SimpleNamespace:
+    """Carga la configuración de correo electrónico desde la base de datos."""
+    logger.trace("Obteniendo configuración de correo electronico desde base de datos.")
+    with current_app.app_context():
         mail_config = database.session.execute(database.select(MailConfig)).first()[0]
 
         # If available, use the configuration from the database
-        flask_app.config["MAIL_SERVER"] = MAIL_SERVER or mail_config.MAIL_SERVER
-        flask_app.config["MAIL_PORT"] = MAIL_PORT or mail_config.MAIL_PORT
-        flask_app.config["MAIL_USE_TLS"] = MAIL_USE_TLS or mail_config.MAIL_USE_TLS
-        flask_app.config["MAIL_USE_SSL"] = MAIL_USE_SSL or mail_config.MAIL_USE_SSL
-        flask_app.config["MAIL_USERNAME"] = MAIL_USERNAME or mail_config.MAIL_USERNAME
-        flask_app.config["MAIL_PASSWORD"] = MAIL_PASSWORD or descifrar_secreto(mail_config.MAIL_PASSWORD)
-        flask_app.config["MAIL_DEFAULT_SENDER"] = MAIL_DEFAULT_SENDER or mail_config.MAIL_DEFAULT_SENDER
+        MAIL_SERVER = mail_config.MAIL_SERVER
+        MAIL_PORT = mail_config.MAIL_PORT
+        MAIL_USE_TLS = mail_config.MAIL_USE_TLS
+        MAIL_USE_SSL = mail_config.MAIL_USE_SSL
+        MAIL_USERNAME = mail_config.MAIL_USERNAME
+        MAIL_PASSWORD = descifrar_secreto(mail_config.MAIL_PASSWORD)
+        MAIL_DEFAULT_SENDER = mail_config.MAIL_DEFAULT_SENDER
+        mail_configured = mail_config.email_verificado
+        mail_enabled = mail_config.email_habilitado
 
-        if DESARROLLO:
-            logger.warning("Opciones de Desarollo activas. Correo electronico deshabilitado.")
-            flask_app.config["MAIL_SUPPRESS_SEND"] = True
-            from now_lms.logs import log
-
-            for key, value in flask_app.config.items():
-                if key.startswith("MAIL_"):
-                    log.debug(f"{key} = {value}")
-
-        return flask_app
-
-
-def send_async_email(mail: Mail, msg: Message):
-    """Función interna que se ejecuta en un subproceso para enviar el email."""
-    app = load_email_setup(current_app)
-
-    with app.app_context():
-        try:
-            mail.send(msg)
-            logger.debug(f"Correo enviado exitosamente a: {msg.recipients}")
-        except Exception as e:
-            logger.error(f"Error al enviar correo a {msg.recipients}: {e}")
+        return SimpleNamespace(
+            mail_configured=mail_configured,
+            mail_enabled=mail_enabled,
+            MAIL_SERVER=MAIL_SERVER,
+            MAIL_PORT=MAIL_PORT,
+            MAIL_USERNAME=MAIL_USERNAME,
+            MAIL_PASSWORD=MAIL_PASSWORD,
+            MAIL_USE_TLS=MAIL_USE_TLS,
+            MAIL_USE_SSL=MAIL_USE_SSL,
+            MAIL_DEFAULT_SENDER=MAIL_DEFAULT_SENDER,
+        )
 
 
-def enviar_correo_asincrono(mail: Mail, mensaje: Message):
-    """
-    Envía un mensaje de correo electrónico de forma asincrónica usando subprocesos.
-
-    :param app: Instancia de Flask configurada.
-    :param mail: Instancia de Flask-Mail.
-    :param mensaje: Instancia de flask_mail.Message.
-    """
+def send_threaded_email(mail: Mail, msg: Message):
+    """Función interna que se ejecuta en un hilo para enviar el email."""
+    logger.trace(f"Enviando correo a {msg.recipients} en segundo plano.")
     try:
-        p = Process(target=send_async_email, args=(mail, mensaje))
-        p.start()
-        logger.debug(f"Subproceso iniciado para enviar email a: {mensaje.recipients}")
+        result = mail.send(msg)
+        logger.trace(f"Correo enviado a {msg.recipients} con respuesta SMTP: {result}")
     except Exception as e:
-        logger.error(f"No se pudo iniciar el subproceso de envío de correo: {e}")
+        logger.error(f"Error al enviar correo a {msg.recipients}: {e}")
+
+
+def _config() -> SimpleNamespace:
+
+    config_from_env = _load_mail_config_from_env()
+
+    if config_from_env.mail_configured:
+        return config_from_env
+    else:
+        return _load_mail_config_from_db()
+
+
+def _mail(config: SimpleNamespace) -> Mail:
+    """Configura y devuelve una instancia de Flask-Mail."""
+
+    app = current_app
+
+    for key, value in vars(config).items():
+        app.config[key] = value
+
+    return mail.init_app(app)
+
+
+def send_mail(msg: Message, background: bool = True):
+    """
+    Envía un mensaje de correo electrónico de forma asincrónica usando hilos.
+
+    :param mail: Instancia de Flask.
+    :param msg: Instancia de flask_mail.Message.
+    :param background: Si es True, envía el correo en segundo plano.
+    """
+
+    config = _config()
+    mail = _mail(config)
+
+    if config.mail_configured and config.mail_enabled:
+        if background:
+            try:
+                hilo = threading.Thread(target=send_threaded_email, args=(mail, msg))
+                hilo.start()
+                logger.debug(f"Hilo iniciado para enviar email a: {msg.recipients}")
+            except Exception as e:
+                logger.error(f"No se pudo iniciar el hilo de envío de correo: {e}")
+        else:
+            result = mail.send(msg)
+            logger.trace(f"Correo enviado a {msg.recipients} con respuesta SMTP: {result}")
+    else:
+        logger.warning("No se ha configurado el correo electrónico.")
