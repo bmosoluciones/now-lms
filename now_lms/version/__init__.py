3bb86374fc147a1da817429bde9f610d11eb40cb
William Moreno <wmoreno@bmogroup.solutions>
2025-07-19 17:25:14 -0600

    release: v0.0.1a20.dev20250719
    Signed-off-by: William Moreno <wmoreno@bmogroup.solutions>

--- now_lms/version/__init__.py
+++ now_lms/version/__init__.py
@@ -53,13 +53,13 @@ PRERELEASE = "a20"
 
 # <--------------------------------------------------------------------------> #
 # Date of release
-REVISION = "20250712"
+REVISION = "20250719"
 
 # <--------------------------------------------------------------------------> #
 # Release string
 # Refences:
 #  - https://peps.python.org/pep-0440/
-# 0.0.1a20.dev20250712
+# 0.0.1a20.dev20250719
 if PRERELEASE:
     VERSION = MAYOR + "." + MENOR + "." + PATCH + PRERELEASE + ".dev" + REVISION
 else:
