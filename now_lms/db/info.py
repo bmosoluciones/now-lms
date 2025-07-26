0b733281e9519334dba005e28fc65fffd47bc692
William Moreno <wmoreno@bmogroup.solutions>
2025-07-26 09:58:02 -0600

    refactor: Finance theme
    Signed-off-by: William Moreno <wmoreno@bmogroup.solutions>

--- now_lms/db/info.py
+++ now_lms/db/info.py
@@ -93,7 +93,7 @@ def lms_info() -> SimpleNamespace:
     Returns a SimpleNamespace with LMS information.
     """
     from sqlalchemy import func
-    from now_lms.db import Curso, Usuario
+    from now_lms.db import Certificado, Curso, EstudianteCurso, Programa, Usuario
 
     courses_count = database.session.execute(database.select(func.count()).select_from(Curso)).scalar_one()
     students_count = database.session.execute(
@@ -105,12 +105,28 @@ def lms_info() -> SimpleNamespace:
     moderators_count = database.session.execute(
         database.select(func.count()).select_from(Usuario).where(Usuario.tipo == "moderator")
     ).scalar_one()
+    
+    # Additional metrics for enhanced user experience
+    enrollments_count = database.session.execute(
+        database.select(func.count()).select_from(EstudianteCurso)
+    ).scalar_one()
+    
+    certificates_count = database.session.execute(
+        database.select(func.count()).select_from(Certificado)
+    ).scalar_one()
+    
+    programs_count = database.session.execute(
+        database.select(func.count()).select_from(Programa)
+    ).scalar_one()
 
     return SimpleNamespace(
         courses_count=courses_count,
         students_count=students_count,
         teachers_count=teachers_count,
         moderators_count=moderators_count,
+        enrollments_count=enrollments_count,
+        certificates_count=certificates_count,
+        programs_count=programs_count,
     )
 
 
