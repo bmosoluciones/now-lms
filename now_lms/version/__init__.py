34269dbb33825b183086249f41d3857231703475
William Moreno <wmoreno@bmogroup.solutions>
2026-01-10 07:08:11 -0600

    release: v1.1.4
    Signed-off-by: William Moreno <wmoreno@bmogroup.solutions>

--- now_lms/version/__init__.py
+++ now_lms/version/__init__.py
@@ -40,7 +40,7 @@ CODE_NAME = "Karla"
 # SemVer (https://semver.org)
 MAYOR = "1"
 MENOR = "1"
-PATCH = "3"
+PATCH = "4"
 
 # <--------------------------------------------------------------------------> #
 # Quick fix
