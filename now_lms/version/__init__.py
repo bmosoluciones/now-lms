631638195b78852b0981c0015ebdc01621ee4393
William Moreno <williamjmorenor@gmail.com>
2026-07-21 15:32:40 +0000

    fix(release): preserve origin main metadata
    Signed-off-by: William Moreno <williamjmorenor@gmail.com>

--- now_lms/version/__init__.py
+++ now_lms/version/__init__.py
@@ -30,7 +30,7 @@ CODE_NAME = "Karla"
 # SemVer (https://semver.org)
 MAYOR = "1"
 MENOR = "2"
-PATCH = "4"
+PATCH = "5"
 
 # <--------------------------------------------------------------------------> #
 # Quick fix
